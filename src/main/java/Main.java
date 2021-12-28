5b10012ecb7347fdea6de9d14f2cab220ba745f0
Meirivaldo <95186840+Meirivaldo@users.noreply.github.com>
2021-12-27 22:21:18 -0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, String> aluno = new HashMap<>();
+
+        aluno.put("Nome", "João");
+        aluno.put("Idade", "17");
+        aluno.put("Media", "8.5");
+        aluno.put("Turma", "3a");
+
+        System.out.println(aluno);
+
+
+        System.out.println(aluno.keySet()); //Retorna o que esta na chave
+        System.out.println(aluno.values()); //Retorna os valores (Collections)
+
+        List<Map<String, String>> listAlunos = new ArrayList<>();
+        listAlunos.add(aluno);
+
+        Map<String, String> aluno2 = new HashMap<>();
+
+        aluno2.put("Nome", "Maria");
+        aluno2.put("Idade", "18");
+        aluno2.put("Media", "8.9");
+        aluno2.put("Turma", "3d");
+
+        listAlunos.add(aluno2);
+
+        System.out.println(listAlunos);
+
+        System.out.println(aluno.containsKey("Nome"));
+
+
+    }
+}
